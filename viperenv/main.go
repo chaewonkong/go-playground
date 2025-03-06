ed3ef373d22d99b9087f0e2c71a7027062054c20
chaewonkong <chaewonkong@gmail.com>
2025-03-06 16:10:32 +0900

    feat: viper read from yml

--- viperenv/main.go
+++ viperenv/main.go
@@ -2,21 +2,42 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/spf13/viper"
 )
 
-func main() {
-	v := viper.New()
-	v.AutomaticEnv()
-
-	p := v.GetString("SERVER_PORT")
-	s := v.GetString("SECRET")
-	log.Printf("port: %s, secret: %s\n", p, s)
+// Config 구조체
+type Config struct {
+	Server struct {
+		Port string `mapstructure:"port"`
+		Mode string `mapstructure:"mode"`
+	} `mapstructure:"server"`
+	Database struct {
+		User     string `mapstructure:"user"`
+		Password string `mapstructure:"password"`
+		Host     string `mapstructure:"host"`
+	} `mapstructure:"database"`
 }
 
-/*
-Pros and cons
+func main() {
+	v := viper.New()
+	configPath, ok := os.LookupEnv("CONFIG_PATH")
+	if !ok {
+		configPath = "config.yml"
+	}
 
-os.GetEnv와 뭐가 다른가?
-*/
+	var c Config
+	v.SetConfigFile(configPath)
+
+	if err := v.ReadInConfig(); err != nil {
+		log.Fatal(err)
+	}
+
+	err := v.Unmarshal(&c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("port: %s, secret: %s\n", c.Server.Port, c.Database.Password)
+}

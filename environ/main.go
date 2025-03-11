f55c96d9060653ecef549175ecc550dd6a4295dc
chaewonkong <chaewonkong@gmail.com>
2025-03-11 10:09:05 +0900

    chore: nested struct

--- environ/main.go
+++ environ/main.go
@@ -15,19 +15,18 @@ func main() {
 	fmt.Printf("Port: %s, DSN: %s\n", cfg.Port, cfg.DSN)
 }
 
-type Config interface {
-	// Keys() []string
-	UnmarshalFromEnviron() error
-}
-
 type config struct {
+	ClusterConfig
 	Port string `env:"PORT"`
 	DSN  string `env:"DSN"`
+}
+
+type ClusterConfig struct {
+	ClusterID string `env:"CLUSTER_ID"`
+	SentryDSN string `env:"SENTRY_DSN"`
 }
 
 func (c *config) UnmarshalFromEnviron() error {
 	_, err := env.UnmarshalFromEnviron(c)
 	return err
 }
-
-var _ Config = (*config)(nil)
